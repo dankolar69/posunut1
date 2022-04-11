295ab53c34ce4d74999ec6d580bfd40cd7ad4be2
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-04-11 23:08:15 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+let a = [0, 0]
+let b = [1, 0]
+let c = [2, 0]
+let d = [3, 0]
+let e = [4, 0]
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+led.plot(c[0], c[1])
+led.plot(d[0], d[1])
+led.plot(e[0], e[1])
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let y = 0
+    while (y < 5) {
+        a[1] += 1
+        b[1] += 1
+        c[1] += 1
+        d[1] += 1
+        e[1] += 1
+        y += 1
+        if (y >= 5) {
+            y = 0
+            a[1] = 0
+            b[1] = 0
+            c[1] = 0
+            d[1] = 0
+            e[1] = 0
+        }
+        
+        basic.clearScreen()
+        led.plot(a[0], a[1])
+        led.plot(b[0], b[1])
+        led.plot(c[0], c[1])
+        led.plot(d[0], d[1])
+        led.plot(e[0], e[1])
+        basic.pause(3000)
+    }
+})
